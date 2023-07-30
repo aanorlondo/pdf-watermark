cb5e73d2ebc70d7735e4ecb5dc72c7b4e02ee508
aanorlondo <khaled.khebbeb@gmail.com>
2023-07-30 22:36:43 +0200

    build: add installation step to apps

--- src/setup.sh
+++ src/setup.sh
@@ -14,8 +14,18 @@ deactivate
 rm -rf .eggs
 rm -rf .venv
 
+# INSTALL
+# _______
+APP_PACKAGE="PdfWatermark.app"
+DIST_PATH="dist/${APP_PACKAGE}"
+INSTALL_PATH="${HOME}/Applications/${APP_PACKAGE}"
+rm -rf "${INSTALL_PATH}"
+cp -rf "${DIST_PATH}/" "${INSTALL_PATH}"
+
 # SUCCESS
 # _______
-APP_NAME="PdfWatermark"
-DIST_PATH="dist/${APP_NAME}.app/Contents/MacOS/${APP_NAME}"
-echo "Congrats ! The app has been built here: ${DIST_PATH}. You can run it on your MacOs system :)"
+echo "INSTALLATION SUCCESS
+The PdfWatermark App has succesfully been built (${DIST_PATH}).
+It has been installed in your User Applications folder: ${INSTALL_PATH} !
+Enjoy It :)
+"
